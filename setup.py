e929693217232c551550bb7aa2993e7c29b0cbc8
Ângelo Lovatto <angelolovatto@gmail.com>
2019-07-09 16:00:14 -0300

    chore: add setup.py
    Signed-off-by: Ângelo Lovatto <angelolovatto@gmail.com>

---
+++ setup.py
@@ -0,0 +1,27 @@
+# pylint: disable=missing-docstring
+from setuptools import setup, find_packages
+
+
+def read_file(file_path):
+    with open(file_path, "r") as file:
+        return file.read()
+
+
+setup(
+    name="gym-cartpole-swingup",
+    version="0.0.1",
+    author="Ângelo G. Lovatto",
+    author_email="angelolovatto@gmail.com",
+    description="A simple, continuous-control environment for OpenAI Gym",
+    long_description=read_file("README.md"),
+    long_description_content_type="text/markdown",
+    license="GNU General Public License v3.0",
+    url="",
+    packages=find_packages(),
+    scripts=[],
+    install_requires=["gym"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
